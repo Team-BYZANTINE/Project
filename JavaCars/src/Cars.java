fe230db8a8dce6e74c07ccd485a690ae3634e8b9
MarkovLyubomir <liubomir.markov@gmail.com>
2014-09-14 16:36:13 +0300

    Update Cars.java

--- JavaCars/src/Cars.java
+++ JavaCars/src/Cars.java
@@ -2,37 +2,54 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-
 public class Cars {
 	public int carX, carY;
-	
+
 	private Random randGenerator;
-	private Box carBox;
-	
-	public void createCars(Cars c){
-		carY = 0;
-		
-		randGenerator = new Random();
-		int rndX = randGenerator.nextInt(5);
-		int y = 0;
-		switch (rndX) {
-		case 0: carX = 20;
-		break;
-		case 1: carX = 40;
-		break;
-		case 2: carX = 60;
-		break;
-		case 3: carX = 80;
-		break;
-		case 4: carX = 100;
-		break;
-		}
+	private Car nextCar;
+
+	public Cars() {
+		nextCar = createCars(Game.cars);
 	}
 	
-	public void drawCars(Graphics g){
+	public Car createCars(Cars c) {
+		randGenerator = new Random();
+
+		int x = randGenerator.nextInt(5);
+		int y = 0;
+
+		switch (x) {
+		case 0:
+			x = 0;
+			break;
+		case 1:
+			x = 20;
+			break;
+		case 2:
+			x = 40;
+			break;
+		case 3:
+			x = 60;
+			break;
+		case 4:
+			x = 80;
+			break;
+		}
+		
+		Car b = new Car(x, y);
+		
+		return b;
+		
+		//ArrayList<Car> carArray = new ArrayList<Car>();
+		//carArray.add(carX, carY);
+	}
+
+	public void drawCars(Graphics g) {
 		int boxSize = 20;
 		g.setColor(Color.green);
-		g.fillRect(carX, carY, boxSize, boxSize + 10);
-		carY++;
+		g.fillRect(nextCar.x, nextCar.y, boxSize, boxSize + 10);
+	}
+	public Car getNewCar(){
+		return nextCar;
 	}
 }
