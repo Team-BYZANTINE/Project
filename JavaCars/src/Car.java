b95f178fd17710365418e09a5671fab047ef98be
MarkovLyubomir <liubomir.markov@gmail.com>
2014-09-15 15:02:11 +0300

    Create Car.java

---
+++ JavaCars/src/Car.java
@@ -0,0 +1,21 @@
+
+public class Car {
+	public int x, y;
+	public static final int BOX_SIZE = 20;
+	
+	public Car(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString(){
+		return "[x=" + x + ",y=" + y + "]";
+	}
+	public boolean equals(Object obj){
+		if(obj instanceof Box){
+			Box b = (Box) obj;
+			return (this.x == b.x && this.y == b.y);
+		}
+		return false;
+	}
+}
