5492b447f0092fc53d260b2e29c96a5bf1193343
MarkovLyubomir <liubomir.markov@gmail.com>
2014-09-17 18:01:55 +0300

    Fixed bug
    User can't leave the field now.

--- Car/src/InputHandler.java
+++ Car/src/InputHandler.java
@@ -10,15 +10,20 @@ public class InputHandler implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-		
+
 		// right x = 1, y = 0
 		if (keyCode == KeyEvent.VK_RIGHT) {
-			Game.userCar.velX += 20;
+			if (Game.userCar.velX <= 205) {
+				Game.userCar.velX += 45;
+			}
 		}
 		// left x = -1, y = 0
 		else if (keyCode == KeyEvent.VK_LEFT) {
-			Game.userCar.velX -= 20;
+			if (Game.userCar.velX >= 95) {
+				Game.userCar.velX -= 45;
+			}
 		}
+
 		if (keyCode == KeyEvent.VK_ESCAPE) {
 			System.exit(0);
 		}
@@ -26,13 +31,11 @@ public class InputHandler implements KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-	
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
 
 	}
 
