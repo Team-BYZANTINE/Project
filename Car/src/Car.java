5dcabe81851918f1249b126e7da96c7265569ec5
radisto <dudurap@abv.bg>
2014-09-17 17:32:11 +0300

    Update Car.java

--- Car/src/Car.java
+++ Car/src/Car.java
@@ -1,4 +1,3 @@
-
 public class Car {
 	public int x, y;
 	public static final int BOX_SIZE = 20;
@@ -6,16 +5,5 @@ public class Car {
 	public Car(int x, int y){
 		this.x = x;
 		this.y = y;
-	}
-	
-	public String toString(){
-		return "[x=" + x + ",y=" + y + "]";
-	}
-	public boolean equals(Object obj){
-		if(obj instanceof Car){
-			Car b = (Car) obj;
-			return (this.x == b.x && this.y == b.y);
-		}
-		return false;
 	}
 }
